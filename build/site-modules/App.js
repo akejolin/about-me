8cf71dca0dc0a6e253ae1fc172291632b990a1cd
akejolin <akejolin@gmail.com>
2020-08-24 15:25:07 +0200

    Saved new version for gh-pages.

---
+++ build/site-modules/App.js
@@ -0,0 +1,9 @@
+import React from '../web_modules/react.js';
+
+function App({}) {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "App"
+  }, "About me - akejolin");
+}
+
+export default App;
---
+++ build/web_modules/react.js
@@ -0,0 +1,2 @@
+import { r as react } from './common/index-609ee1d4.js';
+export { r as default } from './common/index-609ee1d4.js';

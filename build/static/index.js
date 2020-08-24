73ef505706ec2fd531310bde0c14a0b19c373ebc
akejolin <akejolin@gmail.com>
2020-08-24 15:00:16 +0200

    Saved new version for gh-pages.

--- build/static/index.js
+++ build/static/index.js
@@ -1,5 +1,1 @@
-import confetti from "../web_modules/canvas-confetti.js";
-confetti.create(document.getElementById("canvas"), {
-  resize: true,
-  useWorker: true
-})({particleCount: 200, spread: 200});
+export default console.log("Hello world!");

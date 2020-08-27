fc0a091055f0b5394c6604f4cff251b7fafe7553
akejolin <akejolin@gmail.com>
2020-08-27 02:00:02 +0200

    Saved new version for gh-pages.

--- build/site-modules/App.js
+++ build/site-modules/App.js
@@ -35,7 +35,10 @@ function App({}) {
   }, /*#__PURE__*/React.createElement("hr", {
     className: "color-first no-margin"
   }), "\xA0")), /*#__PURE__*/React.createElement("div", {
-    className: "row"
+    className: "row",
+    style: {
+      marginBottom: '4rem'
+    }
   }, /*#__PURE__*/React.createElement("div", {
     className: "two offset-by-one columns"
   }, /*#__PURE__*/React.createElement("img", {
@@ -72,7 +75,7 @@ function App({}) {
     className: "color-first"
   }, "Leadership "), /*#__PURE__*/React.createElement("p", null, "I have many years of experience of engineering management, leadership, mentorship. I have had the opportunity to build both small and big teams. I have had wage responsibility, been scrum master, run and led agile software development."), /*#__PURE__*/React.createElement("h6", null, /*#__PURE__*/React.createElement("cite", null, "Leading a team is about carefully listen to the team members, putting the trust to them, their skills, their knowledge and give them space to contribute. Then you point a direction.")), /*#__PURE__*/React.createElement("span", {
     className: "category-title"
-  }, "Most important when I practice SCRUM:"), /*#__PURE__*/React.createElement("br", null), "What i think is absolutely most important when practice SCRUM is to work very ", /*#__PURE__*/React.createElement(K, null, "close to the production"), " environment. If you are comfortable and do truly know how the production environment works and how it is set up, you will be able to solve problems faster, see the solution faster, be more confidence to take quicker decisions, take more correct decisions etc. All that comes together with the result of a lower ", /*#__PURE__*/React.createElement(K, null, "Time To Market (TTM)"), " .", /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("a", {
+  }, "Most important when practice SCRUM:"), /*#__PURE__*/React.createElement("br", null), "What i think is absolutely most important when practice ", /*#__PURE__*/React.createElement(K, null, "SCRUM"), " is to work ", /*#__PURE__*/React.createElement(K, null, "close to the production"), " environment. If you are comfortable and do truly know how the production environment works and how it is set up, you will be able to solve problems faster, see the solutions faster, be more confidence to take quicker and correct decisions. Close to production also means you need to have ", /*#__PURE__*/React.createElement(K, null, "continuous integration"), " in place. You need to run short iteration loops and have very simple and fast pipelines .", /*#__PURE__*/React.createElement("br", null), "All that comes together with the result of less ", /*#__PURE__*/React.createElement(K, null, "Time To Market (TTM)"), " and less stressful situations for the team members.", /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("a", {
     href: "https://www.scrumalliance.org/",
     title: "scrumalliance.org",
     target: "_blank"
@@ -95,6 +98,28 @@ function App({}) {
   }, /*#__PURE__*/React.createElement("div", {
     className: "eight offset-by-two columns"
   }, /*#__PURE__*/React.createElement("div", {
+    className: "card"
+  }, /*#__PURE__*/React.createElement("h5", null, "Moving blocket.se to the cloud"), /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("img", {
+    src: "/about-me/media/png/aws-migration.png",
+    style: {
+      width: '100%'
+    }
+  }), /*#__PURE__*/React.createElement("span", {
+    className: "category-title"
+  }, "Year: "), " 2020", /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("span", {
+    className: "category-title"
+  }, "Description:"), "A major project for my team during the first half year of 2020. My main participation of this project was to move and deal with all ", /*#__PURE__*/React.createElement(K, null, "frontend application"), ". Before we could do the move, the existing apps needed to be adjusted and ", /*#__PURE__*/React.createElement(K, null, "dockerized"), ". They needed to be configurated and migrated to fit the cloud and the ", /*#__PURE__*/React.createElement(K, null, "Kubernetes clusters"), ".", /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("br", null), "Another part of the project was to ", /*#__PURE__*/React.createElement(K, null, "onboard"), " the teams with all the new tools and features that came along with this cloud solution. But we also needed to deal with this major ", /*#__PURE__*/React.createElement(K, null, "culture change"), " this migration evoked when going towards a true ", /*#__PURE__*/React.createElement(K, null, "micro service"), " architecture.")), /*#__PURE__*/React.createElement("div", {
+    className: "card"
+  }, /*#__PURE__*/React.createElement("h5", null, "Decouple the frontend web layer from the backend for blocket.se"), /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("img", {
+    src: "/about-me/media/png/decouple-fe-be.png",
+    style: {
+      width: '100%'
+    }
+  }), /*#__PURE__*/React.createElement("span", {
+    className: "category-title"
+  }, "Year: "), " 2019", /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("span", {
+    className: "category-title"
+  }, "Description:"), "The main target and goal with this project was to decouple the frontend web layer and set it free from the backend. The idea was that the frontend web layer should completely get its data via ", /*#__PURE__*/React.createElement(K, null, "REST APIs"), ", in the same way the native apps did. To do that we needed to rebuild the frontend layer from scratch and turn it from being based on ", /*#__PURE__*/React.createElement(K, null, "C-code"), " to be based on ", /*#__PURE__*/React.createElement(K, null, "Node"), " and ", /*#__PURE__*/React.createElement(K, null, "javascript"), ". We also wanted to merge the mobile web app and the desktop app to be one with features such ", /*#__PURE__*/React.createElement(K, null, "responsive design"), ". We built the web app with framework such ", /*#__PURE__*/React.createElement(K, null, "react"), ", ", /*#__PURE__*/React.createElement(K, null, "redux"), " and ", /*#__PURE__*/React.createElement(K, null, "webpack"), " etc.")), /*#__PURE__*/React.createElement("div", {
     className: "card"
   }, /*#__PURE__*/React.createElement("h5", null, "Asteroids Game"), /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("a", {
     href: "https://www.lindebros.com/asteroids",
@@ -126,13 +151,47 @@ function App({}) {
     className: "column"
   }, /*#__PURE__*/React.createElement("hr", {
     className: "color-first no-margin"
-  }), "akejolin \xA9 2020. ", /*#__PURE__*/React.createElement("a", {
+  }), /*#__PURE__*/React.createElement("a", {
     href: "https://www.linkedin.com/in/jonaslindebros/",
     target: "_blank"
-  }, "LinkedIn"), ", ", /*#__PURE__*/React.createElement("a", {
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "/about-me/media/SVG/linkedin.svg",
+    style: {
+      width: '2rem'
+    }
+  })), ", ", /*#__PURE__*/React.createElement("a", {
     href: "https://github.com/akejolin",
     target: "_blank"
-  }, "Github")))));
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "/about-me/media/SVG/github.svg",
+    style: {
+      width: '2rem'
+    }
+  })), " ", /*#__PURE__*/React.createElement("a", {
+    href: "https://github.com/jonaslindebros",
+    target: "_blank"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "/about-me/media/SVG/github.svg",
+    style: {
+      width: '2rem'
+    }
+  })), /*#__PURE__*/React.createElement("a", {
+    href: "https://stackoverflow.com/users/10405406/j-lindebro",
+    target: "_blank"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "/about-me/media/SVG/stack-overflow.svg",
+    style: {
+      width: '2rem'
+    }
+  })), /*#__PURE__*/React.createElement("a", {
+    href: "https://www.npmjs.com/~akejolin",
+    target: "_blank"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "/about-me/media/SVG/npm.svg",
+    style: {
+      width: '4rem'
+    }
+  })), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("br", null), "akejolin \xA9 2020."))));
 }
 
 export default App;
